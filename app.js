00da2ced2e81ce3bd076aa7b6a641b337f541a41
Joseph (Joe) Hill <joseph.hill@DFRFRM025072.local>
2016-03-19 20:52:30 -0400

    Express Server Static

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-var app = angular.module("portfolioApp", []);
-app.controller("testCtrl", function($scope){
+var app = angular.module("portfolioApp", ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/main.html',
+            controller  : 'mainCtrl'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutCtrl'
+        });
+});
+
+
+
+
+
+app.controller("mainCtrl", function($scope){
+	$scope.message = "Hello Main";
+
+});
+
+app.controller("aboutCtrl", function($scope){
+	$scope.message = "Hello About";
 
 });
