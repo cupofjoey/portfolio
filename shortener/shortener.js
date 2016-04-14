2b1baf95d7120d60cebb2de1d07568f27dd1cc79
Joe Hill <joe@jkh3.com>
2016-04-13 21:05:24 -0400

    Last one for the night, added shortener to this one

--- shortener/shortener.js
+++ shortener/shortener.js
@@ -1,3 +1,20 @@
 app.controller("shortenerCtrl", function($scope, $http) {
   $scope.message = "Hello Shortener!"
+  var dummyurl = "https://url-shortjoehill.herokuapp.com/urls/new?fullUrl=http://www.google.com"
+
+
+  $scope.testFunction = function(){
+  	$http.get(dummyurl).then(function(response) {
+  		console.log(response);
+  	}, function(error) {
+  		console.log(error);
+  	})
+  };
+
 });
+
+
+
+
+
+// https://url-shortjoehill.herokuapp.com/urls/new?fullUrl=http://test.com
